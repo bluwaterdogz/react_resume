c640a6bc825f88d2fd1bc580cd7f3c51127b5891
bluwaterdogz <ps@macbooks-MacBook-Pro.local>
2018-01-03 12:44:02 -0800

    final fix hopefully!

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 8192,
-          name:"assets/required/[name].[ext]"
+          name:"/assets/required/[name].[ext]"
         }
       }
     ]
